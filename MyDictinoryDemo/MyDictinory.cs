d33245c32a0d79816568024fc3624dda50bda0f4
Sadettin Ömer <eskiciomer01@gmail.com>
2021-01-31 23:57:25 +0300

    Add project files.

---
+++ MyDictinoryDemo/MyDictinory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictinoryDemo
+{
+    class MyDictinory<K, V>
+    {
+        K[] keys;
+        V[] values;
+        public MyDictinory()
+
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+
+        public void Add(K key, V value)
+        {
+
+            K[] tempKey = keys; //geçici liste
+            V[] tempValue = values; //geçici liste
+
+            keys = new K[keys.Length + 1];
+            values = new V[values.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+                values[i] = tempValue[i];
+            }
+
+            keys[keys.Length-1] = key;
+            values[values.Length-1] = value;
+            
+        }
+
+        public void GetAll()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine(keys[i] + "==>" + values[i]);
+            }
+        }
+    }
+}
---
+++ MyDictinoryDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictinoryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictinory<string, string> enTr = new MyDictinory<string, string>();
+            enTr.Add("pencil", "Kalem");
+            enTr.Add("Book", "Kitap");
+            enTr.Add("Plate", "Tabak");
+            enTr.Add("Table", "Masa");
+            enTr.Add("School", "Okul");
+            enTr.Add("Car", "Araba");
+
+            enTr.Write();
+        }
+    }
+}
